3ade0c1f60a728404324a07291e6d40efd1eddef
Hankscr459 <defcw58@gmail.com>
2021-02-18 14:38:04 +0800

    query read_single

--- models/Post.php
+++ models/Post.php
@@ -30,7 +30,7 @@
                 p.author,
                 p.created_at
                 FROM
-                ' . $this->table . ' P
+                ' . $this->table . ' p
                 LEFT JOIN
                     categories c ON p.category_id = c.id
                 ORDER BY
@@ -42,5 +42,43 @@
             $stmt->execute();
 
             return $stmt;
+        }
+
+        // Get Single Post
+        public function read_single() {
+            // Create query
+            $query = 'SELECT
+                c.name as category_name,
+                p.id,
+                p.category_id,
+                p.title,
+                p.body,
+                p.author,
+                p.created_at
+                FROM
+                ' . $this->table . ' p
+                LEFT JOIN
+                    categories c ON p.category_id = c.id
+                WHERE
+                    p.id = ?
+                limit 0,1';
+
+                // Prepare statement
+                $stmt = $this->conn->prepare($query);
+
+                // Bind ID
+                $stmt->bindParam(1, $this->id);
+
+                // Execute query
+                $stmt->execute();
+
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                // Set properties
+                $this->title = $row['title'];
+                $this->body = $row['body'];
+                $this->author = $row['author'];
+                $this->category_id = $row['category_id'];
+                $this->category_name = $row['category_name'];
         }
     }
