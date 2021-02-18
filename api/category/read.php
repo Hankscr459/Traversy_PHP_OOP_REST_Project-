720d187f4000883508c7c8a2a9ccfec175cfe7e7
Hankscr459 <defcw58@gmail.com>
2021-02-18 16:53:56 +0800

    api read category

---
+++ api/category/read.php
@@ -0,0 +1,47 @@
+<?php 
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+
+  include_once '../../config/Database.php';
+  include_once '../../models/category.php';
+
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate blog category object
+  $category = new Category($db);
+
+  // Blog Category query
+  $result = $category->read();
+  // Get row count
+  $num = $result->rowCount();
+
+  // Check if any categories
+  if($num > 0) {
+    // Cat array
+    $cat_arr = array();
+    $cat_arr['data'] = array();
+
+    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    extract($row);
+
+    $cat_item = array(
+        'id' => $id,
+        'name' => $name
+    );
+
+    // Push to "data"
+    array_push($cat_arr['data'], $cat_item);
+    }
+
+    // Turn to JSON & output
+    echo json_encode($cat_arr);
+
+  } else {
+    // No Posts
+    echo json_encode(
+      array('message' => 'No Posts Found')
+    );
+  }
