91b76fd4d989bd1dc8ae9bb37aae7d5c67c95fd3
Hankscr459 <defcw58@gmail.com>
2021-02-18 14:20:23 +0800

    api get read_all

--- config/Database.php
+++ config/Database.php
@@ -2,7 +2,7 @@
     class Database {
         private $host = 'localhost';
         private $db_name = 'myblog';
-        private $username = 'root';
+        private $username = 'hank';
         private $password = '123456';
         private $conn;
 
@@ -10,7 +10,7 @@
         public function connect() {
             $this->conn = null;
             try {
-                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname= ' . $this->db_name, 
+                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, 
                 $this->username, $this->password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
